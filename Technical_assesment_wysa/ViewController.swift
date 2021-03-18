08a7424272414c33911f9813e459bc9a8eba976b
s4rv4d <sarvadshetty@gmail.com>
2021-03-18 09:46:06 +0300

    Initial Commit

---
+++ Technical_assesment_wysa/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Technical_assesment_wysa
+//
+//  Created by Sarvad Shetty on 18/03/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
